ad552a8872232360079edcc7bb741d9d21b86e10
Aylin Sayharman <aylin@boomset.com>
2020-09-20 22:57:20 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <br />
+        <button class="btn btn-primary" @click="selectedTemplate = 'appBlue'">Load Blue Template</button>
+        <button class="btn btn-success" @click="selectedTemplate = 'appGreen'">Load Green Template</button>
+        <button class="btn btn-danger" @click="selectedTemplate = 'appRed'">Load Red Template</button>
+        <hr />
+        <component :is="selectedTemplate"></component>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Blue from "./components/Blue";
+import Green from "./components/Green";
+import Red from "./components/Red";
+
+export default {
+  components: {
+    appBlue: Blue,
+    appGreen: Green,
+    appRed: Red,
+  },
+  data() {
+    return {
+      selectedTemplate: "appBlue",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
+
---
+++ src/components/Green.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>Here is the green content</p>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+div {
+  border: 1px solid green;
+  background-color: lightgreen;
+  padding: 30px;
+  margin: 20px auto;
+  text-align: center;
+}
+</style>
